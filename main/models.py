9f1b1811caeece4085904be7db09242519bc6624
David <davidguramisvili@gmail.com>
2023-10-14 22:02:57 +0300

    fix

--- main/models.py
+++ main/models.py
@@ -3,18 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 class UserType(models.Model):
     name = models.CharField("Имя", max_length=15)
-    code = models.CharField("Код", max_length=15)
-    def __str__(self) -> str:
-        return self.title
+    code = models.CharField("Код", max_length=15) 
     class Meta:
         verbose_name= 'Тип пользователя'
         verbose_name_plural = "Типы пользователей"
 
 class User(AbstractUser):
     user_type = models.ForeignKey(UserType, verbose_name="Тип пользователя", on_delete=models.SET_NULL, blank=True, null=True)
-    phone_number = models.CharField("Телефон", max_length=15, blank=True, null=True)
-    def __str__(self) -> str:
-        return self.title
+    phone_number = models.CharField("Телефон", max_length=15, blank=True, null=True)   
     class Meta:
         verbose_name= 'Пользователь'
         verbose_name_plural = "Пользователи"
@@ -22,8 +18,6 @@ class User(AbstractUser):
 class Grade(models.Model):
     number = models.IntegerField("Класс")
     letter = models.CharField("Буква класса", max_length=1)
-    def __str__(self) -> str:
-        return self.title
     class Meta:
         verbose_name= 'Класс'
         verbose_name_plural = "Классы"
@@ -32,8 +26,7 @@ class Student(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE, unique=True)
     grade = models.ForeignKey(Grade, verbose_name="Класс", on_delete=models.CASCADE)
     level_of_knowledge = models.IntegerField("Уровень знаний")
-    def __str__(self) -> str:
-        return self.title
+
     class Meta:
         verbose_name= 'Ученик'
         verbose_name_plural = "Ученики"
@@ -52,16 +45,12 @@ class News(models.Model):
 class Subject(models.Model):
     name = models.CharField("Предмет", max_length=255)
     code = models.CharField("Код", max_length=255)
-    def __str__(self) -> str:
-        return self.title
     class Meta:
         verbose_name= 'Предмет'
         verbose_name_plural = "Предметы"
 
 class Teacher(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE, unique=True)
-    def __str__(self) -> str:
-        return self.title
     class Meta:
         verbose_name= 'Учитель'
         verbose_name_plural = "Учителя"
@@ -69,8 +58,6 @@ class Teacher(models.Model):
 class TeacherSubject(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, verbose_name="Предмет", on_delete=models.CASCADE)
-    def __str__(self) -> str:
-        return self.title
     class Meta:
         verbose_name= 'Предмет учителя'
         verbose_name_plural = "Предметы учителей"
@@ -78,8 +65,6 @@ class TeacherSubject(models.Model):
 class StudentParent(models.Model):
     student = models.ForeignKey(Student, verbose_name="Ученик", on_delete=models.CASCADE)
     parent = models.ForeignKey(User, verbose_name="Родитель", on_delete=models.CASCADE)
-    def __str__(self) -> str:
-        return self.title
     class Meta:
         verbose_name= 'Родитель ученика'
         verbose_name_plural = "Родители учеников"
@@ -91,8 +76,6 @@ class Task(models.Model):
     answer = models.CharField("Ответ", max_length=255)
     subject = models.ForeignKey(Subject, verbose_name="Предмет", on_delete=models.CASCADE)
     is_neural = models.BooleanField("Написано нейросетью?", default=False)
-    def __str__(self) -> str:
-        return self.title
     class Meta:
         verbose_name= 'Задание'
         verbose_name_plural = "Задания"
@@ -106,9 +89,7 @@ class StudentTask(models.Model):
     teacher_check = models.BooleanField("Нужна проверка?", default=False)
     teaher_comment = models.TextField("Комментарий учителя")
     get_date = models.DateTimeField("Дата выдачи", auto_now_add=True)
-    limite_date = models.DateTimeField("Лимит выполнения") 
-    def __str__(self) -> str:
-        return self.title
+    limite_date = models.DateTimeField("Лимит выполнения")
     class Meta:
         verbose_name= 'Задание ученика'
         verbose_name_plural = "Задания учеников"
